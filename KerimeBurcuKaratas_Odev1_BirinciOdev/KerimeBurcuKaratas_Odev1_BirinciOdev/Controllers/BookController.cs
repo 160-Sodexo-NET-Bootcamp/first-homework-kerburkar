730f2f2375d774a47b89f09a322f19e1b257b011
kerbukar <kerimeburcu@gmail.com>
2021-12-20 23:46:04 +0300

    Project Created.

---
+++ KerimeBurcuKaratas_Odev1_BirinciOdev/KerimeBurcuKaratas_Odev1_BirinciOdev/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KerimeBurcuKaratas_Odev1_BirinciOdev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
