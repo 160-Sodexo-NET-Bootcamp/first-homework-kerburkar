52b4173278e5d81616def3b60806605c8fdd1f03
kerbukar <kerimeburcu@gmail.com>
2021-12-21 01:03:52 +0300

    GetAll and GetById created.

--- KerimeBurcuKaratas_Odev1_BirinciOdev/KerimeBurcuKaratas_Odev1_BirinciOdev/Controllers/BookController.cs
+++ KerimeBurcuKaratas_Odev1_BirinciOdev/KerimeBurcuKaratas_Odev1_BirinciOdev/Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KerimeBurcuKaratas_Odev1_BirinciOdev.Data;
+using KerimeBurcuKaratas_Odev1_BirinciOdev.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KerimeBurcuKaratas_Odev1_BirinciOdev.Controllers
@@ -7,5 +9,45 @@ namespace KerimeBurcuKaratas_Odev1_BirinciOdev.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private static DemoDataService demoDataService = new DemoDataService();    
+
+        public BookController()
+        {
+              
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = demoDataService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetByIdFromRoute/{id}")]
+        public IActionResult GetByIdFromRoute([FromRoute] int id)
+        {
+            var result = demoDataService.GetById(id);
+            return Ok(result);
+        }
+        
+        
+        [HttpGet]
+        [Route("GetByIdFromQuery")]
+        public IActionResult GetByIdFromQuery([FromQuery] int id)
+        {
+            var result = demoDataService.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add([FromBody] Book book)
+        {
+            var result = demoDataService.Add(book);
+            return Ok(result);
+        }
+
     }
 }
